4f273277b3122b9f80973286ec459a159d7fa3bd
maguirre <maguirre@optimeconsulting.com>
2023-08-22 11:28:35 -0500

    Probando imagenes

--- src/components/ListOfBooks.jsx
+++ src/components/ListOfBooks.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, View } from 'react-native'
 import { useGetBooks } from '../hooks/useGetBooks'
 import Animated, { Layout } from 'react-native-reanimated'
 import { Book } from './Book'
 import { Title } from './ui/Title'
 
-export function ListOfBooks () {
+export function ListOfBooks() {
   const { books, booksCount } = useGetBooks()
 
   return (
@@ -13,9 +13,26 @@ export function ListOfBooks () {
       <Title styles={{ marginVertical: 10 }}>{booksCount} Libros disponibles</Title>
       <Animated.ScrollView contentContainerStyle={styles.booksContainer}>
         {books.map(book => (
-          <Book key={book.title} book={book}/>
+          <Book key={book.title} book={book} />
         ))}
       </Animated.ScrollView>
+      <ScrollView contentContainerStyle={[styles.booksContainer, {
+        flexDirection: 'column',
+        paddingTop: 30,
+      }]}>
+        {books.map(book => (
+          <View key={book.title} style={{
+            borderWidth: 2,
+            borderColor: 'green',
+            padding: 10,
+            gap: 10,
+            flexDirection: 'row',
+          }}>
+            <Image resizeMode="cover" src={book.cover} style={{ width: 100, height: 160 }} />
+            <Image src={book.cover} style={{ width: 100, height: 160 }} />
+          </View>
+        ))}
+      </ScrollView>
     </View>
   )
 }
